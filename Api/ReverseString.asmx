81977154bf647484b10e886fd48049a607e8e1bf
priyankasethi <topriyanka.info@gmail.com>
2019-03-01 12:33:58 +1100

    Add project files.

---
+++ Api/ReverseString.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ReverseString.asmx.cs" Class="Api.ReverseString" %>
---
+++ Api/ReverseString.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Api
+{
+    /// <summary>
+    /// Summary description for ReverseString
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ReverseString : System.Web.Services.WebService
+    {
+       
+        [WebMethod(MessageName = "ReverseString", Description = "Reverse the input sentence ")]
+        public  string GetReverseString(string Sentence)
+        {
+            return StringHelper.ReverseString(Sentence);
+        }
+
+
+        static class StringHelper
+        {
+            /// <summary>
+            /// Receives string and returns the string with its letters reversed.
+            /// </summary>
+            public static string ReverseString(string s)
+            {
+                char[] arr = s.ToCharArray();
+                Array.Reverse(arr);
+                return new string(arr);
+            }
+        }
+
+
+
+    }
+}
